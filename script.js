1f0ca0ad80d02d2b4a7d88c53b27ab53c70c0706
Gachio <bellotaophyc@outlook.com>
2022-02-09 12:49:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,5 @@ let button =
  document.querySelector("button");
 let card = document.querySelector(".card");
   button.addEventListener("click", () => {
-   console.log("boss")
   card.classList.add("away");
 })
